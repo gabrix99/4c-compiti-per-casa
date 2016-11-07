38bcf79e9e465cd367985b065546cf6e4c22f024
gabrix99 <gabriele.fugagnoli@libero.it>
2016-11-07 13:01:36 +0100

    car.

---
+++ es41.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+float verdure;
+float frutta;
+float pesci;
+float carni;
+float dolci;
+
+
+void aquisizione();
+float media();
+
+int main()
+{
+   aquisizione();
+   cout<<"la media degli incassi giornalieri è: " <<media();
+ return 0;
+}
+
+void aquisizione()
+{
+  cout<<"inserire l'incasso del reparto verdure: ";
+  cin>>verdure;
+  cout<<"inserire l'incasso del reparto frutta: ";
+  cin>>frutta;
+  cout<<"inserire l'incasso del reparto pesci: ";
+  cin>>pesci;
+  cout<<"inserire l'incasso del reparto carni: ";
+  cin>>carni;
+  cout<<"inserire l'incasso del reparto dolci: ";
+  cin>>dolci;
+}
+
+float media()
+{
+    float media;
+    media=(verdure+frutta+pesci+carni+dolci)/5;
+    return media;
+}
